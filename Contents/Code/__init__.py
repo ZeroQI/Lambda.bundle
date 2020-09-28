81ef5a4c8490f67e30727adc810c7452b8f66c53
ZeroQI <benjamin.brisson@gmail.com>
2020-09-28 20:55:44 +0100

    IndentationError: unexpected indent
      File "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Lambda.bundle/Contents/Code/__init__.py", line 403
        Log.Info("Genres:      {}".format(genres     ))
        ^
    IndentationError: unexpected indent
    
    https://github.com/ZeroQI/Lambda.bundle/issues/22

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -400,7 +400,7 @@ def Update(metadata, media, lang, force, agent_type):
               for tag in xml.iterdescendants('Genre'     ):  SaveFile(tag.get('tag'), path, 'movies_nfo', nfo_xml=nfo_xml, xml_field='genre',      metadata_field=metadata.genres,      multi=True, tag_multi='genre');  genres.append(tag.get('tag'))
               for tag in xml.iterdescendants('Collection'):  SaveFile(tag.get('tag'), path, 'movies_nfo', nfo_xml=nfo_xml, xml_field='collection', metadata_field=metadata.collections, multi=True                   );  collections.append(tag.get('tag'))
               for tag in xml.iterdescendants('Role'      ):  SaveFile(tag.get('tag'), path, 'movies_nfo', nfo_xml=nfo_xml, xml_field={'actor': {'role': {'text': tag.get('role')}, 'name': {'text': tag.get('tag')}, 'thumb': {'text': tag.get('thumb')} if 'thumb' in tag else None }}, multi='actor', tag_multi='name');  roles.append(tag.get('tag'))
-                Log.Info("Genres:      {}".format(genres     ))
+              Log.Info("Genres:      {}".format(genres     ))
               Log.Info("Collections: {}".format(collections))
               Log.Info("Roles:       {}".format(roles      ))
               
